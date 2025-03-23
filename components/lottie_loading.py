03fbd8b0e8b3c3bc46206f5a76506a84faebd631
Yeisson Ochoa <yeisson.ochoa72@gmail.com>
2025-03-23 11:07:18 -0500

    Cambio acceso a archivos locales para imágenes y lottie JSON

--- components/lottie_loading.py
+++ components/lottie_loading.py
@@ -3,7 +3,7 @@ from streamlit_lottie import st_lottie_spinner
 
 def create_lotti_loading():
     # Open file by loading
-    with open("animations\\Lottie-Loading.json", "r",errors='ignore') as f:
+    with open("animations/Lottie-Loading.json", "r",errors='ignore') as f:
         data = json.load(f)
 
     # Return lottie loading
--- views/projects_view.py
+++ views/projects_view.py
@@ -10,7 +10,7 @@ def show_projects():
             "", 
             "https://colab.research.google.com/drive/1Ss76BKaG2H37dKBhQo6VpO7ImKt-m3DE", 
             "card_1",
-            "images\\image-generate.png"
+            "images/image-generate.png"
         )
 
         create_card(
@@ -18,7 +18,7 @@ def show_projects():
             "",
             "https://colab.research.google.com/drive/1tGUHCCeCXJKuM_VU3fGAdk6GlD3--nw4",
             "card_2",
-            "images\\transcription.png"
+            "images/transcription.png"
         )
     with col2:
         create_card(
@@ -26,7 +26,7 @@ def show_projects():
             "",
             "https://colab.research.google.com/drive/1y4nA4Vaqm_TFcVBGD_2gFJrdoosaWe6a",
             "card_3",
-            "images\\text-to-speech.png"
+            "images/text-to-speech.png"
         )
 
         create_card(
@@ -34,5 +34,5 @@ def show_projects():
             "",
             "https://colab.research.google.com/drive/1Ia8Izo5nDmpOjJyiYs-xdPyx8-OxtlfK",
             "card_4",
-            "images\\recognition.png"
+            "images/recognition.png"
         )
